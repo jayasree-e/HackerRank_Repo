3e254b72df7f21643463ba66f841eb587f8ce58a
Jayasree E <jayasree.e@techaspect.com>
2018-10-04 16:56:52 +0530

    Added Source Code for Challenge Migratory Birds

---
+++ Day_10/migratorybirds.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the migratoryBirds function below.
+function migratoryBirds(arr) {
+
+    var max = 0;
+    var typemax;
+    for(var type = 1; type <= 5; type++) {
+        var frequency = 0;
+        for(var i in arr) {
+            if(arr[i] == type) {
+                frequency++;
+            }
+        }
+        if(frequency > max) {
+            max = frequency;
+            typemax = type;
+        }
+    }
+    return typemax;
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const arrCount = parseInt(readLine().trim(), 10);
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    const result = migratoryBirds(arr);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
