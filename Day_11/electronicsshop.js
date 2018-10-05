1bb0502c1d66efc9038ba29847cf7ed2663318ec
Jayasree E <jayasree.e@techaspect.com>
2018-10-05 12:33:32 +0530

    Added source code for challenge electronics shop

---
+++ Day_11/electronicsshop.js
@@ -0,0 +1,77 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(str => str.trim());
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the getMoneySpent function below.
+ */
+function getMoneySpent(keyboards, drives, b) {
+    /*
+     * Write your code here.
+     */
+    var max =0;
+    var flag = 0;
+    for(var i=0; i<keyboards.length; i++) {
+        for(var j=0; j<drives.length; j++) {
+            if(keyboards[i]+drives[j] <= b){
+                if(keyboards[i]+drives[j]>max) {
+                    max = keyboards[i]+drives[j];
+                    flag = 1;
+                }
+               
+               }
+        }
+    }
+    if(flag == 1){
+       return max;
+       }
+    else{
+        return -1;
+    }
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const bnm = readLine().split(' ');
+
+    const b = parseInt(bnm[0], 10);
+
+    const n = parseInt(bnm[1], 10);
+
+    const m = parseInt(bnm[2], 10);
+
+    const keyboards = readLine().split(' ').map(keyboardsTemp => parseInt(keyboardsTemp, 10));
+
+    const drives = readLine().split(' ').map(drivesTemp => parseInt(drivesTemp, 10));
+
+    /*
+     * The maximum amount of money she can spend on a keyboard and USB drive, or -1 if she can't purchase both items
+     */
+
+    let moneySpent = getMoneySpent(keyboards, drives, b);
+
+    ws.write(moneySpent + "\n");
+
+    ws.end();
+}
