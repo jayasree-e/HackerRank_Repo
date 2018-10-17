1cdd7f9b1e4afa361f3c576c366711317ec0d11c
jayasree <jayas8sree@gmail.com>
2018-10-17 10:45:34 +0530

    Added Source Code for Challenge Max Min

---
+++ Day_18/maxmin.js
@@ -0,0 +1,62 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the maxMin function below.
+function maxMin(k, arr) {
+ 
+    arr.sort(function (a,b) { return a-b;});
+    console.log(arr);
+    var min = arr[arr.length-1]
+    for(var i = 0,j=k-1; j<arr.length; i++,j++) {
+        var temp = arr[j] - arr[i];
+        if(temp < min) {
+           min = temp;
+           }
+    }
+    return min;
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    const k = parseInt(readLine(), 10);
+
+    let arr = [];
+
+    for (let i = 0; i < n; i++) {
+        const arrItem = parseInt(readLine(), 10);
+        arr.push(arrItem);
+    }
+
+    const result = maxMin(k, arr);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
